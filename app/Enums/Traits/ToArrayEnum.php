85f62a3ef18460828a0f2ed5cf0a73f9048ccf60
smlrods <smlrodrigues.dev@gmail.com>
2023-11-14 13:28:39 -0300

    Add `ToArrayEnum` trait

---
+++ app/Enums/Traits/ToArrayEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums\Traits;
+
+trait ToArrayEnum {
+    public static function toArray(): array
+    {
+        $array = [];
+        foreach (self::cases() as $case) {
+            $array[$case->name] = $case->value;
+        }
+        return $array;
+    }
+}
