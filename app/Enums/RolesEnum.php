f2e09bde5868b5d9bce91b3eb3e520cfd6e18308
smlrods <smlrodrigues.dev@gmail.com>
2023-11-14 13:28:08 -0300

    Add Enum Roles

---
+++ app/Enums/RolesEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\Traits\ToArrayEnum;
+
+enum RolesEnum: string
+{
+    use ToArrayEnum;
+
+    case ADMINISTRATOR = 'administrator';
+    case SALES = 'sales-representative';
+    case SUPPORT = 'customer-support';
+    case MARKETING = 'marketing-professional';
+    case ANALYST = 'analyst';
+
+    public function label(): string
+    {
+        return match ($this) {
+            static::ADMINISTRATOR => 'Administrators',
+            static::SALES => 'Sales Representatives',
+            static::SUPPORT => 'Customer Supports',
+            static::MARKETING => 'Marketing Professionals',
+            static::ANALYST => 'Analysts',
+        };
+    }
+}
