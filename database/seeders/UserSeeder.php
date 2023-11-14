275bf997eb62e2d63109fd68084a667657458e58
smlrods <84884629+smlrods@users.noreply.github.com>
2023-11-14 14:01:59 -0300

    Add User seeder
    - Created a seeder to generate user data for testing and development.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\RolesEnum;
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::factory()
+            ->count(10)
+            ->has(Project::factory()->count(3)->hasTasks(3))
+            ->create();
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => '12345678',
+        ])->assignRole(RolesEnum::ADMINISTRATOR);
+    }
+}
