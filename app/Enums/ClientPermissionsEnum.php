2063ff02f02767e012f3099d9dc4aa5e6492f300
smlrods <smlrodrigues.dev@gmail.com>
2023-11-14 13:29:31 -0300

    Add Client Enum Permissions

---
+++ app/Enums/ClientPermissionsEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\Traits\ToArrayEnum;
+
+enum ClientPermissionsEnum: string
+{
+    use ToArrayEnum;
+
+    case READ_CLIENTS = 'read-clients';
+    case CREATE_CLIENTS = 'create-clients';
+    case EDIT_CLIENTS = 'edit-clients';
+    case UPDATE_CLIENTS = 'update-clients';
+    case DELETE_CLIENTS = 'delete-clients';
+}
