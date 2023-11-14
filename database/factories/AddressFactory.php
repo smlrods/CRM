ea6ab190ef1848306a403c6d740d423c51e11e22
smlrods <smlrodrigues.dev@gmail.com>
2023-11-14 14:06:38 -0300

    Add Address Factory
    - Implemented a factory for generating Address model instances.

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'street_address' => fake()->streetAddress(),
+            'city' => fake()->city(),
+            'state' => fake()->state(),
+            'zip_code' => fake()->postcode(),
+        ];
+    }
+}
