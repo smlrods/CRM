5d054314439b719132232a1f23e3081f3567c6b5
smlrods <84884629+smlrods@users.noreply.github.com>
2023-11-14 14:05:57 -0300

    Add Client Factory
    - Implemented a factory for generating Client model instances.

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Address;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'company' => fake()->company(),
+            'vat' => fake()->unique()->numberBetween(1000, 99999),
+            'address' => Address::factory(),
+        ];
+    }
+}
