5f407b695bcd00042e3ed6d08470cbb4202550f0
smlrods <84884629+smlrods@users.noreply.github.com>
2023-11-14 14:06:58 -0300

    Add Task Factory
    - Implemented a factory for generating Task model instances.

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->word(),
+            'description' => fake()->text(),
+            'due_date' => fake()->dateTimeBetween('+1 day', '+1 week'),
+            'status' => fake()->randomElement([
+                "Not Started",
+                "In Progress",
+                "On Hold",
+                "Completed",
+                "Delayed",
+                "Blocked",
+                "Cancelled",
+                "Needs Review",
+                "High Priority",
+                "Low Priority",
+            ]),
+        ];
+    }
+}
