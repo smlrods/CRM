12b6ede30060d4005eaa42f834b1b214178e7813
smlrods <84884629+smlrods@users.noreply.github.com>
2023-11-14 14:14:13 -0300

    Add User Permissions enum
    - Introduced an enum to handle permissions associated with users.

---
+++ app/Enums/UserPermissionsEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use App\Enums\Traits\ToArrayEnum;
+
+enum UserPermissionsEnum: string
+{
+    use ToArrayEnum;
+
+    case READ_USERS= 'read-users';
+    case CREATE_USERS = 'create-users';
+    case UPDATE_USERS = 'update-users';
+    case DELETE_USERS = 'delete-users';
+}
