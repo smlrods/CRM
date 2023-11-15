190dc4904589e806767aef9f5ba0b9dbe968a924
smlrods <smlrodrigues.dev@gmail.com>
2023-11-15 09:56:36 -0300

    Add User Policy
    - Implemented a policy for managing permissions related to User models.

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserPermissionsEnum;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can(UserPermissionsEnum::READ_USERS);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        return $user->can(UserPermissionsEnum::READ_USERS);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can(UserPermissionsEnum::CREATE_USERS);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        //
+        return $user->can(UserPermissionsEnum::UPDATE_USERS);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        return $user->can(UserPermissionsEnum::DELETE_USERS);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return false;
+    }
+}
