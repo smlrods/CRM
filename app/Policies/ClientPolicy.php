1e9e527d0d534f360b1dca3648bc26a14564ef8b
smlrods <84884629+smlrods@users.noreply.github.com>
2023-11-15 09:21:22 -0300

    Add Client Policy
    - Implemented a policy for managing permissions related to Client
    models.

---
+++ app/Policies/ClientPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\ClientPermissionsEnum;
+use App\Models\Client;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ClientPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can(ClientPermissionsEnum::READ_CLIENTS);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Client $client): bool
+    {
+        return $user->can(ClientPermissionsEnum::READ_CLIENTS);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->can(ClientPermissionsEnum::CREATE_CLIENTS);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Client $client): bool
+    {
+        return $user->can(ClientPermissionsEnum::UPDATE_CLIENTS);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Client $client): bool
+    {
+        return $user->can(ClientPermissionsEnum::DELETE_CLIENTS);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Client $client): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Client $client): bool
+    {
+        return false;
+    }
+}
