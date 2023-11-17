a656a9f19e9ba5fefe03b134b48a9ea5c886f7af
smlrods <smlrodrigues.dev@gmail.com>
2023-11-17 15:28:54 -0300

    Install Flowbite for enhanced styling and UI
    This commit installs Flowbite to improve the overall look and feel of
    the application. The integration includes support for various styling
    elements and UI components. The imperative mood is used in the subject
    line, and the body provides additional details about the purpose and
    scope of the integration.
    
    - Enhances user interface aesthetics
    - Introduces new styling elements
    - Improves overall user experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -17,5 +20,8 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('flowbite/plugin')
+    ],
 };
