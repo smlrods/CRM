48706f0e126d9a434e05fc0e2a20669ceee5d97d
smlrods <84884629+smlrods@users.noreply.github.com>
2023-12-07 12:21:37 -0300

    Add DonutChartDataGenerator trait to Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\DonutChartDataGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,7 +10,7 @@ use Laravel\Scout\Searchable;
 
 class Task extends Model
 {
-    use HasFactory, Searchable;
+    use HasFactory, Searchable, DonutChartDataGenerator;
 
     protected $fillable = ['title', 'description', 'due_date', 'status', 'project_id'];
 
