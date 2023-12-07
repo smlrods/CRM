155a3a29cef44674d7c53bff2b019a546ca3cfce
smlrods <smlrodrigues.dev@gmail.com>
2023-12-07 12:21:25 -0300

    Add DonutChartDataGenerator trait to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Traits\DonutChartDataGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,7 +11,7 @@ use Laravel\Scout\Searchable;
 
 class Project extends Model
 {
-    use HasFactory, Searchable;
+    use HasFactory, Searchable, DonutChartDataGenerator;
 
     public $timestamps = false;
 
