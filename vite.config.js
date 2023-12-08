9d95d2cbd79cbf7520b96364e973a06afe94de8f
smlrods <smlrodrigues.dev@gmail.com>
2023-12-08 12:01:59 -0300

    Add dashboard charts to Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/dashboard/charts/bar_users.js',
+                'resources/js/dashboard/charts/bar_clients.js',
+                'resources/js/dashboard/charts/donut_projects.js',
+                'resources/js/dashboard/charts/donut_tasks.js'
             ],
             refresh: true,
         }),
