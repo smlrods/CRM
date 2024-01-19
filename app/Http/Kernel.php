5ae4eb2131f8039dbcb694ca4f58e8734b963d59
smlrods <smlrodrigues.dev@gmail.com>
2024-01-19 16:43:37 -0300

    Add 'check_organitation' middleware to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check_organitation' => \App\Http\Middleware\CheckOrganizations::class,
     ];
 }
