d87c11f7e05170abc7bc23f4b867c139da9eb76e
smlrods <84884629+smlrods@users.noreply.github.com>
2024-01-19 10:33:18 -0300

    Add Lead model

---
+++ app/Models/Lead.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Lead extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'contact_id',
+        'company_id',
+        'source',
+        'created_at',
+        'status',
+        'description',
+        'organization_id'
+    ];
+
+    public $timestamps = false;
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+}
