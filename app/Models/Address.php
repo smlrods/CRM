da2a83a7240b871d29bef4db6c0a49266e099a49
smlrods <84884629+smlrods@users.noreply.github.com>
2024-01-19 10:28:04 -0300

    Add companies relationship and fullAddress attribute to Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -12,8 +13,15 @@ class Address extends Model
 
     protected $fillable = ['street_address', 'city', 'state', 'zip_code'];
 
-    public function clients(): HasMany
+    public $timestamps = false;
+
+    public function companies(): HasMany
     {
-        return $this->hasMany(Client::class);
+        return $this->hasMany(Company::class);
+    }
+
+    protected function fullAddress(): Attribute
+    {
+        return Attribute::make(get: fn () => "{$this->street_address}, {$this->city}, {$this->state} {$this->zip_code}");
     }
 }
