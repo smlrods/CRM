e77589143b235cb92cd0b84975adece85d135af0
smlrods <smlrodrigues.dev@gmail.com>
2024-03-03 09:39:26 -0300

    Remove dispatch from Registered event

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,7 +47,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        //event(new Registered($user));
 
         Auth::login($user);
 
